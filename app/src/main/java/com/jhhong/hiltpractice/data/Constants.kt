e7fff4002ed14fe12e97babf32a18f01abbc1c50
JH <cky68081@naver.com>
2022-10-10 21:36:23 +0900

    first commit

---
+++ app/src/main/java/com/jhhong/hiltpractice/data/Constants.kt
@@ -0,0 +1,6 @@
+package com.jhhong.hiltpractice.data
+
+object Constants {
+    const val BASE_URL = "https://api.odcloud.kr/api/"
+    const val AUTH_KEY = "4KrGxfou%2BOZXXdhPzt9orS%2FR8a4BniJAHk8idm9AQayC1WDI6JYDwlOD8upIsDz0kRTEHMwbzKoFNNr%2FEFlb0Q%3D%3D"
+}
